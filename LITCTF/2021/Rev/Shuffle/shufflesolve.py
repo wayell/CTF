c96c8e06335c9e57262e681dbd71fac0af00fd1f
wayell <87632451+wayell@users.noreply.github.com>
2021-07-19 14:08:14 +0800

    Update shufflesolve.py

--- LITCTF/2021/Rev/Shuffle/shufflesolve.py
+++ LITCTF/2021/Rev/Shuffle/shufflesolve.py
@@ -6,5 +6,5 @@ def decrypt(message, key):
     random.shuffle(l)
     return "".join(message[i] for i, x in sorted(enumerate(l), key=lambda x: x[1]))
 
-for i in range(0, 1001):
+for i in range(0, 1000):
     print(decrypt('zftr}__g5y_ee0y1{graua00n1l', i))
